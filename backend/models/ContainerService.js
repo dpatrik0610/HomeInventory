72d0f7df69c26bfcf8007f4f96e760f59f989cf0
dpatrik0610 <d.patrik0610@gmail.com>
2023-10-29 21:00:31 +0100

    Created Container Service with basic CRUD functions.

---
+++ backend/models/ContainerService.js
@@ -0,0 +1,36 @@
+const { ObjectId } = require('mongodb');
+
+class ContainerService {
+  constructor(db) {
+    this.containers = db.collection('containers');
+  }
+
+  async createContainer(container) {
+    const result = await this.containers.insertOne(container);
+    return result.ops[0];
+  }
+
+  async getContainers() {
+    return this.containers.find().toArray();
+  }
+
+  async getContainerById(id) {
+    return this.containers.findOne({ _id: new ObjectId(id) });
+  }
+
+  async updateContainer(id, updatedContainer) {
+    const filter = { _id: new ObjectId(id) };
+    const updateDoc = {
+      $set: updatedContainer,
+    };
+    const result = await this.containers.updateOne(filter, updateDoc);
+    return result.modifiedCount > 0;
+  }
+
+  async deleteContainer(id) {
+    const result = await this.containers.deleteOne({ _id: new ObjectId(id) });
+    return result.deletedCount > 0;
+  }
+}
+
+module.exports = ContainerService;
