43ee8618f6f3d6a255ebb4fd16bf70886199076b
FakanBalint <fakan.balint@gmail.com>
2023-10-29 20:00:44 +0100

    created front end branch

---
+++ frontend/vue-app/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <home-inventory-app></home-inventory-app>
+  </div>
+</template>
+
+<script>
+import HomeInventoryApp from "./HomeInvenoryApp.vue";
+
+export default {
+  name: "App",
+  components: {
+    HomeInventoryApp,
+  },
+};
+</script>
