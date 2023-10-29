16f9e0d312fbcf529dae255258866ac66a69f784
FakanBalint <fakan.balint@gmail.com>
2023-10-29 18:02:39 +0100

    modified the App so the buttons are always bellow the items

--- frontend/vue-app/src/HomeInvenoryApp.vue
+++ frontend/vue-app/src/HomeInvenoryApp.vue
@@ -2,11 +2,6 @@
   <div class="flex bg-gray-100 h-screen">
     <div class="w-1/4 p-4 bg-gray-200">
       <div>
-        <div class="mb-4">
-          <button @click="addContainer" class="bg-blue-500 text-white p-2 rounded">
-            + Add Container
-          </button>
-        </div>
         <ul>
           <li v-for="(container, index) in containers" :key="index">
             <button @click="selectContainer(index)" class="p-2 text-blue-500 hover:text-blue-700">
@@ -14,25 +9,31 @@
             </button>
           </li>
         </ul>
+        <div>
+          <button @click="addContainer" class="bg-blue-500 text-white p-2 rounded">
+            + Add Container
+          </button>
+        </div>
       </div>
     </div>
     <div class="w-3/4 p-4">
       <div v-if="selectedContainer">
         <h2 class="text-2xl font-bold">{{ selectedContainer.name }}</h2>
-        <div>
-          <button @click="addItem" class="bg-green-500 text-white p-2 rounded">
-            + Add Item
-          </button>
-        </div>
         <ul>
           <li v-for="(item, index) in selectedContainer.items" :key="index">
             <div class="bg-white rounded p-2 shadow-md mb-2">{{ item.name }}</div>
           </li>
         </ul>
+        <div>
+          <button @click="addItem" class="bg-green-500 text-white p-2 rounded">
+            + Add Item
+          </button>
+        </div>
       </div>
     </div>
   </div>
 </template>
+
 
 <script>
 export default {
