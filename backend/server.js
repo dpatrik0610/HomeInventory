54c0579121804da9973150bbfec82d4addb4080b
dpatrik0610 <d.patrik0610@gmail.com>
2023-10-30 00:34:52 +0100

    Updated app with CORS

--- backend/server.js
+++ backend/server.js
@@ -27,18 +27,7 @@ function setupApp(){
   app.use(express.static("staticfiles"));
   app.use(customLogger);
   
-  const allowedOrigins = ['85.67.13.248'];
-  app.use(cors({
-      origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-          callback(null, true);
-        } else {
-          callback(new Error('Not allowed by CORS'));
-        }
-      },
-      credentials: true,
-      })
-  );
+  app.use(cors());
 
   // Routes
   app.use(express.json());
@@ -49,5 +38,4 @@ function setupApp(){
   app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
-  
 }
