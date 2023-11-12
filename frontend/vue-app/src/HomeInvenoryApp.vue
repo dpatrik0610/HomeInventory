be7e9fac9e11ff3b8b80f71f7ab9b6cc5b395490
dpatrik0610 <d.patrik0610@gmail.com>
2023-11-12 02:25:53 +0100

    Fixed expiration date

--- frontend/vue-app/src/HomeInvenoryApp.vue
+++ frontend/vue-app/src/HomeInvenoryApp.vue
@@ -35,49 +35,56 @@
       </nav>
     </div>
 
-    <!-- Items -->
-    <div class="w-3/4 p-4">
-      <div v-if="$store.selectedContainer">
-        <h2 class="text-2xl font-bold mb-3">{{ $store.selectedContainer.name }}</h2>
-        <ul>
-          <li v-for="(item, index) in $store.displayeditems" :key="index">
-            <div
-              class="flex justify-between items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-gray-800 hover-bg-opacity-80 focus-bg-opacity-80 active-bg-opacity-80 focus-text-blue-900 active-text-blue-900 outline-none m-1"
-            >
-              <span>{{ item.name }}&nbsp;&nbsp;x {{ item.qtty }} &nbsp;&nbsp;{{ item.expiration_date }}</span>
-              <div class="flex">
-                <button class="text-gray-500 mr-2" @click="toggleDropdown(index)">
-                  <i class="fa-solid fa-arrow-right-arrow-left"></i>
-                  <div v-if="item.showDropdown">
-                    <ul class="absolute mt-2 py-2 bg-gray-900 hover:text-gray border border-gray-200 rounded-lg shadow-lg z-10">
-                      <li v-for="(container, cIndex) in $store.containers" :key="cIndex">
-                        <div v-if="container !== $store.selectedContainer" class="cursor-pointer p-2 hover:bg-gray-500 hover:text-white m-1 rounded-lg" @click="moveToContainer(index, cIndex)">
-                          {{ container.name }}
-                        </div>
-                      </li>
-                    </ul>
-                  </div>
-                </button>
-                <button class="text-blue-500 mr-2" @click="editItemName(index)">
-                  <i class="fa-solid fa-pen-to-square"></i>
-                </button>
-                <button class="text-red-500" @click="deleteItem(index)">
-                  <i class="fa-solid fa-trash-can"></i>
-                </button>
-              </div>
+  <!-- Items -->
+  <div class="w-3/4 p-4">
+    <div v-if="$store.selectedContainer">
+      <h2 class="text-2xl font-bold mb-3">{{ $store.selectedContainer.name }}</h2>
+      <ul>
+        <li v-for="(item, index) in $store.displayeditems" :key="index">
+          <div
+            class="flex justify-between items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-gray-800 hover-bg-opacity-80 focus-bg-opacity-80 active-bg-opacity-80 focus-text-blue-900 active-text-blue-900 outline-none m-1"
+          >
+            <span>
+              {{ item.name }}&nbsp;&nbsp;x {{ item.qtty }}
+              <!-- Conditionally render expiration date -->
+              <template v-if="item.expiration_date">
+                &nbsp;&nbsp;( Expires at: {{ item.expiration_date }} )
+              </template>
+            </span>
+            <div class="flex">
+              <button class="text-gray-500 mr-2" @click="toggleDropdown(index)">
+                <i class="fa-solid fa-arrow-right-arrow-left"></i>
+                <div v-if="item.showDropdown">
+                  <ul class="absolute mt-2 py-2 bg-gray-900 hover:text-gray border border-gray-200 rounded-lg shadow-lg z-10">
+                    <li v-for="(container, cIndex) in $store.containers" :key="cIndex">
+                      <div v-if="container !== $store.selectedContainer" class="cursor-pointer p-2 hover:bg-gray-500 hover:text-white m-1 rounded-lg" @click="moveToContainer(index, cIndex)">
+                        {{ container.name }}
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </button>
+              <button class="text-blue-500 mr-2" @click="editItemName(index)">
+                <i class="fa-solid fa-pen-to-square"></i>
+              </button>
+              <button class="text-red-500" @click="deleteItem(index)">
+                <i class="fa-solid fa-trash-can"></i>
+              </button>
             </div>
-          </li>
-        </ul>
-        <div
-          role="button"
-          tabindex="0"
-          class="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-green-900 hover-bg-opacity-80 focus-bg-opacity-80 active-bg-opacity-80 focus-text-blue-900 active-text-blue-900 outline-none border border-green-500"
-          @click="addItem"
-        >
-          + Add Item
-        </div>
+          </div>
+        </li>
+      </ul>
+      <div
+        role="button"
+        tabindex="0"
+        class="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-green-900 hover-bg-opacity-80 focus-bg-opacity-80 active-bg-opacity-80 focus-text-blue-900 active-text-blue-900 outline-none border border-green-500"
+        @click="addItem"
+      >
+        + Add Item
       </div>
     </div>
+  </div>
+
   </div>
 </template>
 
