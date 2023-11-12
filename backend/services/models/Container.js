1775d9ce9fb5b008e7da8c5f0a577cdec53ab7fe
dpatrik0610 <d.patrik0610@gmail.com>
2023-11-12 16:59:58 +0100

    Input validation

--- backend/services/models/Container.js
+++ backend/services/models/Container.js
@@ -1,5 +1,6 @@
 class Container {
   constructor(name, description = null) {
+    if(name.length > 30 || description.length > 300) return;
     this.name = name;
     this.description = description;
   }
