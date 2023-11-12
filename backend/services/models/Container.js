21f19c1600ecc63eadb32df9ca29bdf4e2548c48
dpatrik0610 <d.patrik0610@gmail.com>
2023-11-12 17:09:58 +0100

    Input validation

--- backend/services/models/Container.js
+++ backend/services/models/Container.js
@@ -1,5 +1,15 @@
 class Container {
   constructor(name, description = null) {
+    // Validate name
+    if (typeof name !== 'string' || name.trim() === '' || name.length > 255) {
+      throw new Error('Name must be a non-empty string with a maximum length of 255 characters');
+    }
+
+    // Validate description if provided
+    if (description !== null && (typeof description !== 'string' || description.length > 1000)) {
+      throw new Error('Description must be a string with a maximum length of 1000 characters');
+    }
+
     this.name = name;
     this.description = description;
   }
--- backend/services/models/Item.js
+++ backend/services/models/Item.js
@@ -2,11 +2,37 @@ const { ObjectId } = require('mongodb');
 
 class Item {
   constructor(containerId, name, qtty = 1, expiration_date = null) {
+    // Validate containerId
+    if (!ObjectId.isValid(containerId)) {
+      throw new Error('Invalid containerId');
+    }
+
+    // Validate name
+    if (typeof name !== 'string' || name.trim() === '' || name.length > 255) {
+      throw new Error('Name must be a non-empty string with a maximum length of 255 characters');
+    }
+
+    // Validate qtty
+    if (typeof qtty !== 'number' || qtty < 1 || qtty > 1000) {
+      throw new Error('Quantity must be a positive number with a maximum value of 1000');
+    }
+
+    // Validate expiration_date if provided
+    if (expiration_date !== null && !isValidDate(expiration_date)) {
+      throw new Error('Invalid expiration_date');
+    }
+
     this.containerId = new ObjectId(containerId);
     this.name = name;
     this.qtty = qtty;
     this.expiration_date = expiration_date;
   }
+}
+
+function isValidDate(dateString) {
+  // You can implement a more sophisticated date validation logic here
+  const regex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z$/;
+  return regex.test(dateString);
 }
 
 module.exports = Item;
