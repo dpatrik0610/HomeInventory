65541a01f5134b477bfcdf2e06a91a964aa5b350
dpatrik0610 <d.patrik0610@gmail.com>
2023-11-12 17:02:12 +0100

    Revert changes

--- backend/services/models/Container.js
+++ backend/services/models/Container.js
@@ -1,6 +1,5 @@
 class Container {
   constructor(name, description = null) {
-    if(name.length > 30 || description.length > 300) return;
     this.name = name;
     this.description = description;
   }
--- backend/services/models/Item.js
+++ backend/services/models/Item.js
@@ -2,7 +2,6 @@ const { ObjectId } = require('mongodb');
 
 class Item {
   constructor(containerId, name, qtty = 1, expiration_date = null) {
-    if(name.length > 30 || qtty > 9999999) return;
     this.containerId = new ObjectId(containerId);
     this.name = name;
     this.qtty = qtty;
